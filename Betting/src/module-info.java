37a4d358a7b7b65cd720aa5c7b87b1740dc72a45
Nicola Tango <128502338+Notkar@users.noreply.github.com>
2023-05-22 15:33:41 +0200

    Add files via upload

---
+++ Betting/src/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * @author Nicola Tango
+ * @author Nunzio Cascone
+ * @author Michelangelo Tundo
+ *
+ *
+ */
+module Betting {
+	requires java.sql;
+	requires java.desktop;
+	requires java.management;
+}
